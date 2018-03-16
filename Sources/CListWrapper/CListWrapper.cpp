e1554ab13b6bcf61f909e5cb6e4c450934977b32
J-Mangia <j.mangia@ibm.com>
2018-03-15 17:28:30 -0700

    Adding C Wrapper to C++ Wrapper .. work in progress

--- Sources/CListWrapper/CListWrapper.cpp
+++ Sources/CListWrapper/CListWrapper.cpp
@@ -7,3 +7,29 @@ long cFactorial(int n) {
     if (n == 0 || n == 1) return 1;
     return n * cFactorial(n-1);
 }
+
+
+
+
+
+
+
+const void * initializeListWrapper() {
+    CPPListWrapper *list = new CPPListWrapper();
+    return (void *)list;
+}
+
+long size(const void *object) {
+    CPPListWrapper *list = (CPPListWrapper *)object;
+    return list->size();
+}
+
+bool empty(const void *object) {
+    CPPListWrapper *list = (CPPListWrapper *)object;
+    return list->empty();
+}
+
+
+
+
+
--- Sources/CListWrapper/include/CListWrapper.hpp
+++ Sources/CListWrapper/include/CListWrapper.hpp
@@ -1,8 +1,29 @@
+#include <stdbool.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
     long cFactorial(int n);
+
+
+    const void * initializeListWrapper();
+    long size(const void *object);
+    bool empty(const void *object);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #ifdef __cplusplus
 }
