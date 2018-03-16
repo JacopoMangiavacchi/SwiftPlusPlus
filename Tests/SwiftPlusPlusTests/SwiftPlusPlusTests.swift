560621cf44e9042f48cb11a5ddfac0d076f5f148
J-Mangia <j.mangia@ibm.com>
2018-03-16 11:00:59 -0700

    Added functional test

--- Tests/SwiftPlusPlusTests/SwiftPlusPlusTests.swift
+++ Tests/SwiftPlusPlusTests/SwiftPlusPlusTests.swift
@@ -7,17 +7,17 @@ class SwiftPlusPlusTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
 
-        let w = SwiftListWrapper(dataSize: 1)
-        w.push_back(value: "1".data(using: .utf8)!)
-        w.push_back(value: "2".data(using: .utf8)!)
-        w.push_back(value: "3".data(using: .utf8)!)
-        w.push_front(value: "0".data(using: .utf8)!)
+        let list = SwiftListWrapper(dataSize: 1)
+        list.push_back(value: "1".data(using: .utf8)!)
+        list.push_back(value: "2".data(using: .utf8)!)
+        list.push_back(value: "3".data(using: .utf8)!)
+        list.push_front(value: "0".data(using: .utf8)!)
 
-        XCTAssertEqual(w.size(), 4)
+        XCTAssertEqual(list.size(), 4)
 
-        XCTAssertEqual(String.init(data: w.front()!, encoding: .utf8), "0")
+        XCTAssertEqual(String.init(data: list.front()!, encoding: .utf8), "0")
 
-        let it = w.begin()
+        let it = list.begin()
 
         let _ = it.increment()
         let _ = it.increment()
